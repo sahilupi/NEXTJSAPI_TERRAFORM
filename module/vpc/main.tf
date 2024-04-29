c222ddaa931fa710535206af1eb551f9adda8640
yashika <yashika@gmail.com>
2024-04-29 23:30:03 +0530

    change line 67

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -63,7 +63,7 @@ resource "aws_route_table_association" "public_rt_tb_association" {
 # Elastic_ip for nat
 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
+  allocation_id = aws_nat_gateway.nat.id
   subnet_id     = element(aws_subnet.public_subnets.*.0)
    depends_on = [aws_internet_gateway.ig]
 
